b7654aea843f0aceccc7a8f661472036e939032b
Ruth Cassidy <ruth.cassidy@zinkworks.com>
2022-05-05 11:01:28 +0100

    Create a web controller for a simple web app

---
+++ src/main/java/com/zinkworks/springpractice/SpringPracticeApp.java
@@ -0,0 +1,20 @@
+package com.zinkworks.springpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringPracticeApp {
+    public static void main (String... args) {
+        SpringApplication.run(
+            SpringPracticeApp.class, args);
+    }
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
