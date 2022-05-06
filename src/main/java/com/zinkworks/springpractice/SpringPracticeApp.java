aea0c5b0c33c90de7b6f7c396dcdcb8bbf199685
Ruth Cassidy <ruth.cassidy@zinkworks.com>
2022-05-06 10:45:48 +0100

    Create controller package and move getMapping method to it

--- src/main/java/com/zinkworks/springpractice/SpringPracticeApp.java
+++ src/main/java/com/zinkworks/springpractice/SpringPracticeApp.java
@@ -2,8 +2,6 @@ package com.zinkworks.springpractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -12,9 +10,5 @@ public class SpringPracticeApp {
     public static void main (String... args) {
         SpringApplication.run(
             SpringPracticeApp.class, args);
-    }
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
     }
 }
