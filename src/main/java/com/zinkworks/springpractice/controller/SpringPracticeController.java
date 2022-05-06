344f009e1eccea32d22b2aaf2b45afecaf96e34e
Ruth Cassidy <ruth.cassidy@zinkworks.com>
2022-05-06 13:08:32 +0100

    Add service annotation

--- src/main/java/com/zinkworks/springpractice/controller/SpringPracticeController.java
+++ src/main/java/com/zinkworks/springpractice/controller/SpringPracticeController.java
@@ -1,13 +1,17 @@
 package com.zinkworks.springpractice.controller;
 
+import com.zinkworks.springpractice.service.MyFirstService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SpringPracticeController {
+    @Autowired
+    private MyFirstService myFirstService;
     @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
+    public String hello(@RequestParam(value = "name", defaultValue = "Jack") String name) {
+        return myFirstService.greetThisPerson(name);
     }
 }
--- src/main/java/com/zinkworks/springpractice/service/Test.java
+++ src/main/java/com/zinkworks/springpractice/service/Test.java
@@ -1,10 +1,12 @@
 package com.zinkworks.springpractice.service;
 
+import org.springframework.stereotype.Service;
+
+@Service("myFirstService")
 public class Test implements MyFirstService {
 
     @Override
     public String greetThisPerson(String name) {
-        System.out.println("Do something " + name);
-        return name;
+        return "Do something " + name;
     }
 }
